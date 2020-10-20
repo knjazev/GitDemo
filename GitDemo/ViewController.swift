c30c19afbf526344c4ed3ca3f527b394e3976e0c
Dmitry Kniazev <knjazev@me.com>
2020-10-20 16:23:01 +0300

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Князев Дмитрий on 10/20/20.
+//  Copyright © 2020 Dmitry Kniazev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
