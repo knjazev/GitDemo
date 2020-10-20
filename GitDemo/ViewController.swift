ff67eab45f6eb05babb6415f53380a6e9941ddd4
Dmitry Kniazev <51081336+knjazev@users.noreply.github.com>
2020-10-21 00:24:30 +0300

    Update ViewController.swift adding print statement

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Another print statement")
-        
+        print ("Hello GitHub")   
     }
-
-
 }
 
